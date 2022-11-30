4f5c3125b4179325711f1cbe6fea40ed2be47586
high-moctane <high.moctane@gmail.com>
2022-12-01 00:00:25 +0900

    back: added graphql-yoga, pothos (close #3)

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,12 +1,58 @@
-import fastify from "fastify";
+import fastify, { FastifyRequest, FastifyReply } from "fastify";
+import SchemaBuilder from "@pothos/core";
+import { createYoga } from "graphql-yoga";
 
-const server = fastify();
+const app = fastify();
 
-server.get("/", async () => {
-  return "Hello Mocjudge!";
+const builder = new SchemaBuilder({});
+
+builder.queryType({
+  fields: (t) => ({
+    hello: t.string({
+      args: {
+        name: t.arg.string(),
+      },
+      resolve: (_parent, { name }) => `hello, ${name || "Mocjudge"}`,
+    }),
+  }),
 });
 
-server.listen({ port: 8438 }, (err, addr) => {
+app.route({
+  url: "/graphql",
+  method: ["GET", "POST", "OPTIONS"],
+  handler: async (req, reply) => {
+    console.log(reply);
+    const response = await yoga.handleNodeRequest(req, {
+      req,
+      reply,
+    });
+    response.headers.forEach((value, key) => {
+      reply.header(key, value);
+    });
+
+    reply.status(response.status);
+
+    reply.send(response.body);
+    console.log(reply);
+
+    return reply;
+  },
+});
+
+const yoga = createYoga<{
+  req: FastifyRequest;
+  reply: FastifyReply;
+}>({
+  schema: builder.toSchema(),
+  logging: {
+    debug: (...args) => args.forEach((arg) => app.log.debug(arg)),
+    info: (...args) => args.forEach((arg) => app.log.info(arg)),
+    warn: (...args) => args.forEach((arg) => app.log.warn(arg)),
+    error: (...args) => args.forEach((arg) => app.log.error(arg)),
+  },
+});
+
+app.listen({ port: 8438 }, (err, addr) => {
   if (err) {
     console.error(err);
     process.exit(1);
